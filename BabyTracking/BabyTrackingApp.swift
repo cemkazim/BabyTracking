1f690a9bf963f6afa1e040151b7ec54d756b69fb
Cem Kazım <cemkazim@hotmail.com>
2022-02-16 18:52:59 +0300

    first commit

---
+++ BabyTracking/BabyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTrackingApp.swift
+//  BabyTracking
+//
+//  Created by Cem Kazım Genel on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BabyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyTracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabyTracking
+//
+//  Created by Cem Kazım Genel on 16/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
